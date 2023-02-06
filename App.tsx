28673c2f64498a02c26622dae0b9f8bd596b71e3
sumer-alhussein <somar.alhussien@gmail.com>
2023-02-06 23:33:00 +0300

    add welcome screen

--- App.tsx
+++ App.tsx
@@ -1,22 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { extendTheme, NativeBaseProvider } from "native-base";
+
 import useCachedResources from "./src/hooks/useCachedResources";
 import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/screens/navigation";
 
+// Screen
+import Welcome from "./src/screens/welcome/welcomeScreen";
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
+type MyThemeType = typeof theme;
+declare module "native-base" {
+  interface ICustomTheme extends MyThemeType {}
+}
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+  // const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+      <NativeBaseProvider>
+        {/* <Navigation colorScheme={colorScheme} /> */}
+        <Welcome />
         <StatusBar />
-      </SafeAreaProvider>
+      </NativeBaseProvider>
     );
   }
 }
